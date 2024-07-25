6d17dcaa9cb586fd10b714c74d738fd9e82eb5ec
bermet <bermet.alibek@gmail.com>
2024-07-24 23:57:03 -0500

    pw removed

---
+++ week2/User.java
@@ -0,0 +1,18 @@
+package week2;
+
+public class User {
+
+    private String name;
+    private String accountType;
+    private int balance;
+    private int accountNumber;
+
+
+        
+  
+
+
+public static void main (String[] args) {
+  System.out.println("Hello");
+}
+}
